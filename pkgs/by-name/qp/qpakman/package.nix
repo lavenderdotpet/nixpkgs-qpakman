0388d7e6ad29468bbee3411764cc527de7309604
lavenderdotpet <51702517+lavenderdotpet@users.noreply.github.com>
2025-10-26 05:07:05 +0200

    test swap to librequake qpakman

--- pkgs/by-name/qp/qpakman/package.nix
+++ pkgs/by-name/qp/qpakman/package.nix
@@ -4,18 +4,18 @@
   fetchFromGitHub,
   pkg-config,
   cmake,
-  libpng,
+  stb,
   zlib,
 }:
 stdenv.mkDerivation {
   pname = "qpakman";
-  version = "0-unstable-2024-03-07";
+  version = "0-unstable-2025-08-30";
 
   src = fetchFromGitHub {
-    owner = "fhomolka";
+    owner = "LibreQuake";
     repo = "qpakman";
-    rev = "5a76df23c11f4da619448c60a1a2ba35c316ec2b";
-    hash = "sha256-DzP1PTzXRn8mKnPzMpxXnN9ZvFPMWWuVedll+FjFcj4=";
+    rev = "cdc3c7b41bd921307938226d386cb14e62a88416";
+    hash = "0000000000000000000000000000000000000000";
   };
 
   nativeBuildInputs = [
@@ -24,12 +24,12 @@ stdenv.mkDerivation {
   ];
 
   buildInputs = [
-    libpng
+    stb
     zlib
   ];
 
   meta = with lib; {
-    homepage = "https://github.com/fhomolka/qpakman";
+    homepage = "https://github.com/LibreQuake/qpakman";
     description = "command-line tool for managing PAK and WAD files from QuakeI/II & Hexen II";
     license = licenses.gpl2Plus;
     maintainers = [ ];
